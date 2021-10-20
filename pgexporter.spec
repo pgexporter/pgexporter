c4718d4e1d4def17679e73affbae2903fd194c8b
jesperpedersen <jesper.pedersen@redhat.com>
2021-10-20 07:57:35 -0400

    Next is 0.3.0

--- pgexporter.spec
+++ pgexporter.spec
@@ -1,5 +1,5 @@
 Name:          pgexporter
-Version:       0.2.0
+Version:       0.3.0
 Release:       1%{dist}
 Summary:       Prometheus exporter for PostgreSQL
 License:       BSD
--- src/include/pgexporter.h
+++ src/include/pgexporter.h
@@ -41,7 +41,7 @@ extern "C" {
 #include <sys/types.h>
 #include <openssl/ssl.h>
 
-#define VERSION "0.2.0"
+#define VERSION "0.3.0"
 
 #define PGEXPORTER_HOMEPAGE "https://pgexporter.github.io/"
 #define PGEXPORTER_ISSUES "https://github.com/pgexporter/pgexporter/issues"
