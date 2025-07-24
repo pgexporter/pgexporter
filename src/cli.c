9a557f39660872251425506f95baa34e71314666
User1995 <userfrom1995@gmail.com>
2025-07-24 18:03:22 +0000

    [#274] Fix inconsistent JSON output format in pgexporter-cli conf get

--- src/cli.c
+++ src/cli.c
@@ -1013,7 +1013,7 @@ process_get_result(SSL* ssl, int socket, char* config_key, int32_t output_format
       if (MANAGEMENT_OUTPUT_FORMAT_JSON == output_format)
       {
          json_res = (struct json*)res;
-         pgexporter_json_print(json_res, FORMAT_JSON_COMPACT);
+         pgexporter_json_print(json_res, FORMAT_JSON);
       }
       else
       {
@@ -1047,7 +1047,7 @@ process_get_result(SSL* ssl, int socket, char* config_key, int32_t output_format
       if (MANAGEMENT_OUTPUT_FORMAT_JSON == output_format)
       {
          json_res = (struct json*)res;
-         pgexporter_json_print(json_res, FORMAT_JSON_COMPACT);
+         pgexporter_json_print(json_res, FORMAT_JSON);
       }
       else
       {
@@ -1205,6 +1205,8 @@ get_config_key_result(char* config_key, struct json* j, uintptr_t* r, int32_t ou
 
    struct json* configuration_js = NULL;
    struct json* filtered_response = NULL;
+   struct json* full_response = NULL;
+   struct json* outcome_section = NULL;
    struct json* response = NULL;
    struct json* outcome = NULL;
    struct json_iterator* iter = NULL;
@@ -1222,8 +1224,14 @@ get_config_key_result(char* config_key, struct json* j, uintptr_t* r, int32_t ou
       return 0;
    }
 
+   if (pgexporter_json_clone(j, &full_response))
+   {
+      goto error;
+   }
+
    if (pgexporter_json_create(&filtered_response))
    {
+      pgexporter_json_destroy(full_response);
       goto error;
    }
 
@@ -1390,18 +1398,18 @@ get_config_key_result(char* config_key, struct json* j, uintptr_t* r, int32_t ou
       goto error;
    }
 
+   pgexporter_json_put(full_response, MANAGEMENT_CATEGORY_RESPONSE, (uintptr_t)filtered_response, ValueJSON);
+
    if (output_format == MANAGEMENT_OUTPUT_FORMAT_JSON || !config_key)
    {
-      *r = (uintptr_t)filtered_response;
-
+      *r = (uintptr_t)full_response;
       free(config_value);
       config_value = NULL;
-
    }
    else
    {
       *r = (uintptr_t)config_value;
-      pgexporter_json_destroy(filtered_response);
+      pgexporter_json_destroy(full_response);
    }
 
    // Clean up parts
@@ -1419,19 +1427,30 @@ get_config_key_result(char* config_key, struct json* j, uintptr_t* r, int32_t ou
 error:
    if (output_format == MANAGEMENT_OUTPUT_FORMAT_JSON)
    {
-      pgexporter_json_put(filtered_response, "Outcome", (uintptr_t)false, ValueBool);
-      *r = (uintptr_t)filtered_response;
-
+      // Update the Outcome section to indicate failure
+      if (full_response)
+      {
+         outcome_section = (struct json*)pgexporter_json_get(full_response, MANAGEMENT_CATEGORY_OUTCOME);
+         if (outcome_section)
+         {
+            pgexporter_json_put(outcome_section, MANAGEMENT_ARGUMENT_STATUS, (uintptr_t)false, ValueBool);
+         }
+         // Set empty response section
+         pgexporter_json_put(full_response, MANAGEMENT_CATEGORY_RESPONSE, (uintptr_t)filtered_response, ValueJSON);
+         *r = (uintptr_t)full_response;
+      }
+      else
+      {
+         pgexporter_json_put(filtered_response, "Outcome", (uintptr_t)false, ValueBool);
+         *r = (uintptr_t)filtered_response;
+      }
       free(config_value);
       config_value = NULL;
-
    }
    else
    {
-
       free(config_value);
       config_value = NULL;
-
       config_value = (char*)malloc(6);
       if (config_value)
       {
@@ -1439,15 +1458,17 @@ error:
       }
       *r = (uintptr_t)config_value;
       pgexporter_json_destroy(filtered_response);
+      if (full_response)
+      {
+         pgexporter_json_destroy(full_response);
+      }
    }
 
    // Clean up parts on error
    for (int i = 0; i < part_count; i++)
    {
-
       free(parts[i]);
       parts[i] = NULL;
-
    }
 
    free(config_key_copy);
